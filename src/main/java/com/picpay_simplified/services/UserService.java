1e8f20c16ef93c37516c690db7d2579f0af2e394
ggarabs <ggarabetti0607@gmail.com>
2025-06-17 21:43:16 -0300

    feat: User services created

---
+++ src/main/java/com/picpay_simplified/services/UserService.java
@@ -0,0 +1,33 @@
+package com.picpay_simplified.services;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.picpay_simplified.domain.user.User;
+import com.picpay_simplified.domain.user.UserType;
+import com.picpay_simplified.repositories.UserRepository;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public void validateTransaction(User payer, BigDecimal amount) throws Exception{
+        if(payer.getUserType() == UserType.MERCHANT){
+            throw new Exception("Logistas não realizam transações.");
+        }
+        if(payer.getBalance().compareTo(amount) < 0){
+            throw new Exception("Saldo insuficiente para realizar transação.");
+        }
+    }
+
+    public User findUserById(Long id) throws Exception{
+        return this.repository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado."));
+    }
+
+    public void saveUser(User user){
+        this.repository.save(user);
+    }
+}
