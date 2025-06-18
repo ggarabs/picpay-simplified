65aefd7aff60f6db0c56d22ec6826a3ab7357fa1
ggarabs <ggarabetti0607@gmail.com>
2025-06-18 00:06:27 -0300

    feat: user service adaptaded to new DTOs and services

--- src/main/java/com/picpay_simplified/services/UserService.java
+++ src/main/java/com/picpay_simplified/services/UserService.java
@@ -1,12 +1,14 @@
 package com.picpay_simplified.services;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.picpay_simplified.domain.user.User;
 import com.picpay_simplified.domain.user.UserType;
+import com.picpay_simplified.dtos.UserDTO;
 import com.picpay_simplified.repositories.UserRepository;
 
 @Service
@@ -29,5 +31,15 @@ public class UserService {
 
     public void saveUser(User user){
         this.repository.save(user);
+    }
+
+    public User createUser(UserDTO user){
+        User newUser = new User(user);
+        this.saveUser(newUser);
+        return newUser;
+    }
+
+    public List<User> getAllUsers(){
+        return this.repository.findAll(); 
     }
 }
