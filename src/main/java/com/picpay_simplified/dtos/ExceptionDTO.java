ff9c2095548297e970a9d1d2c61e8cfcfdf7d4e5
ggarabs <ggarabetti0607@gmail.com>
2025-06-19 15:39:38 -0300

    feat: created basic exception handling

---
+++ src/main/java/com/picpay_simplified/dtos/ExceptionDTO.java
@@ -0,0 +1,4 @@
+package com.picpay_simplified.dtos;
+
+public record ExceptionDTO(String message, String statusCode) {
+}
---
+++ src/main/java/com/picpay_simplified/infra/ControllerExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.picpay_simplified.infra;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.picpay_simplified.dtos.ExceptionDTO;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity threatGeneralException(Exception exception){
+        ExceptionDTO exceptionDTO = new ExceptionDTO(exception.getMessage(), "500");
+        return ResponseEntity.internalServerError().body(exceptionDTO);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity threatDuplicateEntry(DataIntegrityViolationException exception){
+        ExceptionDTO exceptionDTO = new ExceptionDTO("Usuário já cadastrado", "400");
+        return ResponseEntity.badRequest().body(exceptionDTO);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity threat404(EntityNotFoundException exception){
+        return ResponseEntity.notFound().build();
+    }
+
+}
